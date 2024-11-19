5aa8d318e59d3ae56798a29b807b667d43f4a893
chami-gunawardane <chamigunawardane@gmail.com>
2024-11-19 23:50:47 +0530

    Deleted backend

---
+++ mongodb-frontend/src/pages/Dashboard/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function index() {
+  return (
+    <div>
+      <h1>Student Management Application with Mongo DB</h1>
+
+      
+    </div>
+  )
+}
+
+export default index
